7bdd57f9c5da85243cbf6b0bc962d02be16545d2
ideanb <iduartdean@gmail.com>
2018-08-17 19:20:33 -0500

    stop on finish line

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const reactions = [];
 const elements = ["bioexpert", "patricia", "panther", "mariposa"];
 
+const finishLine = document.querySelector(".finish-line");
+let finished = false;
+let winner = "";
+
 for (i = 0; i < elements.length; i++) {
   const element = document.querySelector(`#${elements[i]}`);
-  reaction.setElement(element);
+  reaction.element = element;
   reactions.push(Object.assign({}, reaction));
 }
 
 const run = () => {
   reactions.forEach(function(r) {
-    r.setVelocityX(Math.random() * 4);
-    r.update();
+    if (r.x >= finishLine.offsetLeft) {
+      finished = true;
+      winner = r.element.getAttribute("id");
+    }
+    if (!finished) {
+      r.vx = Math.random() * 4;
+      r.update();
+    }
   });
   requestAnimationFrame(run);
 };
--- reaction.js
+++ reaction.js
@@ -1,34 +1,9 @@
 const reaction = {
-  width: 200,
-  height: 50,
   x: 0,
   y: 0,
   vx: 0,
-  vy: 0,
   direction: 1,
   element: "<div />",
-  setElement: function(element) {
-    this.element = element;
-  },
-  setPosition: function(x, y) {
-    this.x = x; 
-    this.y = y;
-  },
-  setPositionX: function(x) {
-    this.x = x;
-  },
-  setPositionY: function(y) {
-    this.y = y;
-  },
-  setVelocityX: function(vx) {
-    this.vx = vx;
-  },
-  draw: function() {
-    const div = this.element
-    div.textContent = this.text;
-    div.style.transform = `translate(${this.x}px, ${this.y}px)`;
-    this.element = div;
-  },
   update: function() {
     this.x += this.vx;
     this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
